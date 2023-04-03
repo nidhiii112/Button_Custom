6235dc5e7a497039e969607e445128e38507bcdf
nidhi singh <nidhii950898@gmail.com>
2023-04-03 23:57:16 +0530

    final commit

---
+++ app/src/main/java/com/nidhi/buttom_custom/MainActivity.java
@@ -0,0 +1,19 @@
+package com.nidhi.buttom_custom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button5);
+
+        btn.setEnabled(false);
+    }
+}
